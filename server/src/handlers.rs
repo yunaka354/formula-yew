46dd74d4e9eac04d4e1d9bff4bc41281d36df29c
yunaka354 <yuichi.nakamura86@gmail.com>
2023-12-27 22:42:47 -0500

    implement standings_handler

--- server/src/handlers.rs
+++ server/src/handlers.rs
@@ -1,4 +1,7 @@
 use axum::{http::StatusCode, Json};
+use ergast_rust::api::{Path, URLParams};
+use ergast_rust::ergast::Ergast;
+use ergast_rust::models::{MRData, StandingTable};
 use serde::Serialize;
 
 // the output to our `create_user` handler
@@ -13,10 +16,18 @@ pub async fn root() -> &'static str {
     "Hello, World"
 }
 
-pub async fn standings_handler() -> (StatusCode, Json<User>) {
-    let user = User {
-        id: 1,
-        username: "test".to_string(),
+// basic handler that responds with a static string
+pub async fn standings_handler(
+) -> Result<(StatusCode, Json<MRData<StandingTable>>), (StatusCode, Json<&'static str>)> {
+    let path = Path {
+        year: 2023,
+        round: Some(1),
     };
-    (StatusCode::OK, Json(user))
+    let params = URLParams::default();
+    let result = Ergast::standings(path, params).await;
+
+    match result {
+        Ok(standings) => Ok((StatusCode::OK, Json(standings))),
+        Err(_) => Err((StatusCode::BAD_REQUEST, Json("error"))),
+    }
 }
