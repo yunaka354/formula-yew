0c304d6c8464b71bb466033c605fa4969ca50b2b
yunaka354 <yuichi.nakamura86@gmail.com>
2024-01-01 22:18:17 -0500

    smaller spinner

--- client/src/components/spinner.rs
+++ client/src/components/spinner.rs
@@ -3,8 +3,6 @@ use yew::prelude::*;
 #[function_component(Spinner)]
 pub fn spinner() -> Html {
     html! {
-        <div class="flex justify-center items-center">
-            <div class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
-        </div>
+        <div class="border-t-transparent border-solid animate-spin rounded-full w-8 h-8 border-4 border-blue-400 border-t-4"></div>
     }
 }
